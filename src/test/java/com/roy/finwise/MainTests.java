1ed7c70e420590069e632db4a1b4da87faf0633e
Sougata Roy <sougata.roy26@yahoo.com>
2025-03-08 12:58:10 +0530

    Initial Commit

---
+++ src/test/java/com/roy/finwise/MainTests.java
@@ -0,0 +1,13 @@
+package com.roy.finwise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
