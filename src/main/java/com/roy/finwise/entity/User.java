6bcc7b9f354d5b4f0d253726db4db13a2844cdb4
Sougata Roy <sougata.roy26@yahoo.com>
2025-03-08 12:59:45 +0530

    Add User Entity

---
+++ src/main/java/com/roy/finwise/entity/User.java
@@ -0,0 +1,50 @@
+package com.roy.finwise.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(unique = true)
+    private String mobileNumber;
+
+    @OneToMany(mappedBy = "user", orphanRemoval = true, cascade = CascadeType.ALL)
+    private Set<Transaction> transactions = new HashSet<>();
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id) && Objects.equals(name, user.name) && Objects.equals(email, user.email) && Objects.equals(password, user.password) && Objects.equals(mobileNumber, user.mobileNumber) && Objects.equals(transactions, user.transactions);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, email, password, mobileNumber, transactions);
+    }
+}
