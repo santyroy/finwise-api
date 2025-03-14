b08348772eaa4bc9fb94fcb8bee87e265ec3a1df
Sougata Roy <sougata.roy26@yahoo.com>
2025-03-14 23:26:17 +0530

    Add User Controller with createUser method

---
+++ src/main/java/com/roy/finwise/controller/UserController.java
@@ -0,0 +1,28 @@
+package com.roy.finwise.controller;
+
+import com.roy.finwise.dto.UserRequest;
+import com.roy.finwise.dto.UserResponse;
+import com.roy.finwise.service.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping
+    public ResponseEntity<UserResponse> createUser(@Valid @RequestBody UserRequest userRequest) {
+        UserResponse user = userService.createUser(userRequest);
+        return ResponseEntity.status(HttpStatus.CREATED).body(user);
+    }
+
+}
