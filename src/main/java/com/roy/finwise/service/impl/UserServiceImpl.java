6e3417f627653489a711758dc3f473e6d9f05abc
Sougata Roy <sougata.roy26@yahoo.com>
2025-03-14 22:42:19 +0530

    Add loggers to create user service method

--- src/main/java/com/roy/finwise/service/impl/UserServiceImpl.java
+++ src/main/java/com/roy/finwise/service/impl/UserServiceImpl.java
@@ -9,24 +9,31 @@ import com.roy.finwise.service.UserService;
 import com.roy.finwise.util.DtoToEntityUtil;
 import com.roy.finwise.util.EntityToDtoUtil;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
+@Slf4j
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
 
     @Override
     public UserResponse createUser(UserRequest userRequest) {
+        log.info("Creating user with email: {}", userRequest.getEmail());
         User newUser = DtoToEntityUtil.userDtoToEntity(userRequest);
         Optional<User> userOpt = userRepository.findByEmail(newUser.getEmail());
         if (userOpt.isPresent()) {
+            log.error("User with email: {} already exist", userRequest.getEmail());
             throw new UserAlreadyExistException("User with email: " + newUser.getEmail() + " already exists");
         }
         User savedUser = userRepository.save(newUser);
+        log.info("User saved with ID: {}", savedUser.getId());
         return EntityToDtoUtil.userEntityToDto(savedUser);
     }
 
