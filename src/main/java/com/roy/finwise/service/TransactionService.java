863f3b265574860b59af9fb8012f4487ba80c9fd
Sougata Roy <sougata.roy26@yahoo.com>
2025-03-15 12:42:27 +0530

    Add CRUD methods in TransactionService

--- src/main/java/com/roy/finwise/service/TransactionService.java
+++ src/main/java/com/roy/finwise/service/TransactionService.java
@@ -2,8 +2,13 @@ package com.roy.finwise.service;
 
 import com.roy.finwise.dto.TransactionRequest;
 import com.roy.finwise.dto.TransactionResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface TransactionService {
 
     TransactionResponse createTransaction(TransactionRequest transactionRequest);
+    Page<TransactionResponse> getAllTransactions(String userId, Pageable pageable);
+    TransactionResponse updateTransaction(String transactionId, TransactionRequest transactionRequest);
+    void deleteTransaction(String transactionId);
 }
