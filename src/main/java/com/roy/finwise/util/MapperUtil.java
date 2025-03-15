eb5a5d03f6f760d1f443a8bfd749f833ac6349e4
Sougata Roy <sougata.roy26@yahoo.com>
2025-03-15 12:09:59 +0530

    Created new MapperUtil class

--- src/main/java/com/roy/finwise/util/MapperUtil.java
+++ src/main/java/com/roy/finwise/util/MapperUtil.java
@@ -1,7 +1,9 @@
 package com.roy.finwise.util;
 
 import com.roy.finwise.dto.TransactionRequest;
+import com.roy.finwise.dto.TransactionResponse;
 import com.roy.finwise.dto.UserRequest;
+import com.roy.finwise.dto.UserResponse;
 import com.roy.finwise.entity.Category;
 import com.roy.finwise.entity.Transaction;
 import com.roy.finwise.entity.TransactionType;
@@ -9,7 +11,7 @@ import com.roy.finwise.entity.User;
 
 import java.time.Instant;
 
-public class DtoToEntityUtil {
+public class MapperUtil {
 
     public static Transaction transactionDtoToEntity(TransactionRequest transactionRequest, Category category, User user) {
         Instant now = Instant.now();
@@ -31,6 +33,26 @@ public class DtoToEntityUtil {
                 .email(userRequest.getEmail())
                 .password(userRequest.getPassword())
                 .mobileNumber(userRequest.getMobileNumber())
+                .build();
+    }
+
+    public static TransactionResponse transactionEntityToDto(Transaction transaction) {
+        return TransactionResponse.builder()
+                .id(transaction.getId())
+                .type(transaction.getType())
+                .amount(transaction.getAmount())
+                .category(transaction.getCategory().getName())
+                .description(transaction.getDescription())
+                .tags(transaction.getTags())
+                .build();
+    }
+
+    public static UserResponse userEntityToDto(User user) {
+        return UserResponse.builder()
+                .userId(user.getId().toString())
+                .name(user.getName())
+                .email(user.getEmail())
+                .mobileNumber(user.getMobileNumber())
                 .build();
     }
 }
