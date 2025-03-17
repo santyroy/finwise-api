53f4892a96eb75245c154d7bf8c861f2598b3caf
Sougata Roy <sougata.roy26@yahoo.com>
2025-03-17 17:34:16 +0530

    Implement HATEOAS way to return Page wise transaction data

--- src/main/java/com/roy/finwise/controller/TransactionController.java
+++ src/main/java/com/roy/finwise/controller/TransactionController.java
@@ -7,6 +7,9 @@ import com.roy.finwise.service.TransactionService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.PagedModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,6 +20,7 @@ import org.springframework.web.bind.annotation.*;
 public class TransactionController {
 
     private final TransactionService transactionService;
+    private final PagedResourcesAssembler<TransactionResponse> pagedResourcesAssembler;
 
     @PostMapping
     public ResponseEntity<TransactionResponse> createTransaction(@Valid @RequestBody TransactionRequest request) {
@@ -55,6 +59,20 @@ public class TransactionController {
         Page<TransactionResponse> allTransactions = transactionService
                 .getAllTransactions(userId, pageNo, pageSize, direction, properties);
         return ResponseEntity.ok(new PageDTO<>(allTransactions));
+    }
+
+    /*
+    Option 2: Option 2: Use Spring HATEOAS (More powerful but requires additional dependency)
+     */
+    @GetMapping("/users/{userId}/hateoas")
+    public PagedModel<EntityModel<TransactionResponse>> getAllTransactionsByPageHATEOAS(@PathVariable String userId,
+                                                                                        @RequestParam(defaultValue = "0") int pageNo,
+                                                                                        @RequestParam(defaultValue = "10") int pageSize,
+                                                                                        @RequestParam(defaultValue = "DESC") String direction,
+                                                                                        @RequestParam(defaultValue = "createdAt") String properties) {
+        Page<TransactionResponse> allTransactions = transactionService
+                .getAllTransactions(userId, pageNo, pageSize, direction, properties);
+        return pagedResourcesAssembler.toModel(allTransactions);
     }
 
     @PutMapping("{transactionId}")
