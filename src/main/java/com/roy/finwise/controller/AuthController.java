8700df0c2309b59d0521838f6f9a988c39821699
Sougata Roy <sougata.roy26@yahoo.com>
2025-03-17 20:52:00 +0530

    Cleanup AuthController

--- src/main/java/com/roy/finwise/controller/AuthController.java
+++ src/main/java/com/roy/finwise/controller/AuthController.java
@@ -4,19 +4,11 @@ import com.roy.finwise.dto.LoginRequest;
 import com.roy.finwise.dto.LoginResponse;
 import com.roy.finwise.dto.UserRequest;
 import com.roy.finwise.dto.UserResponse;
-import com.roy.finwise.security.service.JwtService;
 import com.roy.finwise.service.AuthService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.InternalAuthenticationServiceException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,32 +19,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AuthController {
 
-    private final AuthenticationManager authManager;
     private final AuthService authService;
-    private final JwtService jwtService;
 
     @PostMapping("/login")
     public ResponseEntity<LoginResponse> login(@Valid @RequestBody LoginRequest request) {
-        try {
-            // Authenticate the user
-            Authentication authentication = authManager.authenticate(
-                    new UsernamePasswordAuthenticationToken(request.email(), request.password()));
-
-            // Set authentication to security context
-            SecurityContextHolder.getContext().setAuthentication(authentication);
-
-            // Generate JWT token
-            String jwt = jwtService.generateToken((UserDetails) authentication.getPrincipal());
-
-            // Get user
-            UserResponse userResponse = authService.getUser(request.email());
-
-            return ResponseEntity.ok(
-                    new LoginResponse(jwt, userResponse.getName(), userResponse.getEmail(), userResponse.getRoles()));
-
-        } catch (BadCredentialsException | InternalAuthenticationServiceException ex) {
-            throw new BadCredentialsException(ex.getMessage());
-        }
+        LoginResponse loginResponse = authService.login(request);
+        return ResponseEntity.ok(loginResponse);
     }
 
     @PostMapping("/signup")
