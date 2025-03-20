54f278f729da1c144b38d61f78821e42b75d7d8f
Sougata Roy <sougata.roy26@yahoo.com>
2025-03-20 17:05:31 +0530

    Add Refresh Token entity and repository

---
+++ src/main/java/com/roy/finwise/entity/RefreshToken.java
@@ -0,0 +1,25 @@
+package com.roy.finwise.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+public class RefreshToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private String token;
+    private Instant expiration;
+
+    @OneToOne
+    private User user;
+}
---
+++ src/main/java/com/roy/finwise/service/RefreshTokenRepository.java
@@ -0,0 +1,12 @@
+package com.roy.finwise.service;
+
+import com.roy.finwise.entity.RefreshToken;
+import com.roy.finwise.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, UUID> {
+    Optional<RefreshToken> findByUser(User user);
+}
