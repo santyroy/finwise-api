7f9ed8e39e8f60b0b3fd281d0ac3199c5e6d896d
Sougata Roy <sougata.roy26@yahoo.com>
2025-03-20 19:40:45 +0530

    Add new method refreshToken in AuthServices

--- src/main/java/com/roy/finwise/service/AuthService.java
+++ src/main/java/com/roy/finwise/service/AuthService.java
@@ -1,11 +1,9 @@
 package com.roy.finwise.service;
 
-import com.roy.finwise.dto.LoginRequest;
-import com.roy.finwise.dto.LoginResponse;
-import com.roy.finwise.dto.UserRequest;
-import com.roy.finwise.dto.UserResponse;
+import com.roy.finwise.dto.*;
 
 public interface AuthService {
     UserResponse registerUser(UserRequest request);
     LoginResponse login(LoginRequest request);
+    RefreshTokenResponse refreshToken(RefreshTokenRequest request);
 }
