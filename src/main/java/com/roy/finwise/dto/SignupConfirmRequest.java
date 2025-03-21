77370cf3bd60d3851230d738be3df7d4dea7cf98
Sougata Roy <sougata.roy26@yahoo.com>
2025-03-21 19:27:57 +0530

    Refactor SignupConfirmRequest

--- src/main/java/com/roy/finwise/dto/SignupConfirmRequest.java
+++ src/main/java/com/roy/finwise/dto/SignupConfirmRequest.java
@@ -2,19 +2,12 @@ package com.roy.finwise.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@NoArgsConstructor
-@AllArgsConstructor
-@Getter
-@Setter
-public class SignupConfirmRequest {
-    @NotBlank(message = "OTP cannot be empty")
-    @Pattern(regexp = "^[0-9]{6}$", message = "OTP must be 6 digits")
-    private String otp;
-    @NotBlank(message = "Email is mandatory")
-    private String email;
+public record SignupConfirmRequest(
+        @NotBlank(message = "OTP cannot be empty")
+        @Pattern(regexp = "^[0-9]{6}$", message = "OTP must be 6 digits")
+        String otp,
+        @NotBlank(message = "Email is mandatory")
+        @Pattern(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$", message = "Incorrect email format")
+        String email) {
 }
