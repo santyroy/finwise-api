c8872375895f5c955e99da27909f862b82fca4cc
Sougata Roy <sougata.roy26@yahoo.com>
2025-04-11 21:03:03 +0530

    Add wallet repository

---
+++ src/main/java/com/roy/finwise/repository/WalletRepository.java
@@ -0,0 +1,9 @@
+package com.roy.finwise.repository;
+
+import com.roy.finwise.entity.Wallet;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface WalletRepository extends JpaRepository<Wallet, UUID> {
+}
