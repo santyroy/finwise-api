bf2d887ed5d27e5a3163ad2face3db402b0ddcea
Sougata Roy <sougata.roy26@yahoo.com>
2025-04-11 21:29:26 +0530

    Update createWallet method to add userId in params

--- src/main/java/com/roy/finwise/service/WalletService.java
+++ src/main/java/com/roy/finwise/service/WalletService.java
@@ -2,13 +2,12 @@ package com.roy.finwise.service;
 
 import com.roy.finwise.dto.WalletRequest;
 import com.roy.finwise.dto.WalletResponse;
-import com.roy.finwise.entity.Wallet;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface WalletService {
-    WalletResponse createWallet(WalletRequest walletRequest);
+    WalletResponse createWallet(WalletRequest walletRequest, String userId);
     Optional<WalletResponse> getWalletById(String walletId);
     List<WalletResponse> getAllWallets();
     WalletResponse updateWallet(String walletId, WalletRequest walletRequest);
