c3ab1838d8c388c455492dc62827961d61f5f969
Sougata Roy <sougata.roy26@yahoo.com>
2025-04-12 16:21:59 +0530

    Update wallet repository to add new method

--- src/main/java/com/roy/finwise/repository/WalletRepository.java
+++ src/main/java/com/roy/finwise/repository/WalletRepository.java
@@ -1,9 +1,12 @@
 package com.roy.finwise.repository;
 
+import com.roy.finwise.entity.User;
 import com.roy.finwise.entity.Wallet;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface WalletRepository extends JpaRepository<Wallet, UUID> {
+    List<Wallet> findByUser(User user);
 }
