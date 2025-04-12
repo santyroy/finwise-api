95211b2a9bb36aef48a9e086724c5e69aa56ae61
Sougata Roy <sougata.roy26@yahoo.com>
2025-04-12 12:11:00 +0530

    Update wallet response DTO

--- src/main/java/com/roy/finwise/dto/WalletResponse.java
+++ src/main/java/com/roy/finwise/dto/WalletResponse.java
@@ -1,8 +1,11 @@
 package com.roy.finwise.dto;
 
+import lombok.Builder;
+
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.Set;
 
+@Builder
 public record WalletResponse(String name, BigDecimal spendingLimits, Set<TransactionResponse> transactions, LocalDateTime createdAt, LocalDateTime updatedAt) {
 }
