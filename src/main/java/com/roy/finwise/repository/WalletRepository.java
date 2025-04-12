7a58163916a01776675f882ef4659850806f8458
Sougata Roy <sougata.roy26@yahoo.com>
2025-04-12 17:48:57 +0530

    Update wallet repository to JOIN FETCH transactions and tags using EntityGraph

--- src/main/java/com/roy/finwise/repository/WalletRepository.java
+++ src/main/java/com/roy/finwise/repository/WalletRepository.java
@@ -2,11 +2,13 @@ package com.roy.finwise.repository;
 
 import com.roy.finwise.entity.User;
 import com.roy.finwise.entity.Wallet;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.UUID;
 
 public interface WalletRepository extends JpaRepository<Wallet, UUID> {
+    @EntityGraph(attributePaths = {"transactions", "transactions.tags"})
     List<Wallet> findByUser(User user);
 }
