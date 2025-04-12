5833f99120d157884d4f60cfb5e420478670aaf7
Sougata Roy <sougata.roy26@yahoo.com>
2025-04-12 19:06:29 +0530

    Update UserRequest dto in order to update user data

--- src/main/java/com/roy/finwise/dto/UserRequest.java
+++ src/main/java/com/roy/finwise/dto/UserRequest.java
@@ -12,18 +12,14 @@ import lombok.*;
 @Builder
 public class UserRequest {
 
-    @NotBlank(message = "Name is mandatory")
     private String name;
 
-    @NotBlank(message = "Email is mandatory")
     @Pattern(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$", message = "Incorrect email format")
     private String email;
 
-    @NotBlank(message = "Password is mandatory")
     @Size(min = 8, message = "Password must of minimum 8 characters")
     private String password;
 
-    @NotBlank(message = "Mobile number is mandatory")
     @Pattern(regexp = "^\\d{10}$", message = "Mobile number must be exactly 10 digits")
     private String mobileNumber;
 
