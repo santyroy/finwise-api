7d4e28289ddde1117f058a17e8580e663c57396b
Sougata Roy <sougata.roy26@yahoo.com>
2025-04-12 16:21:40 +0530

    Update wallet service method signatures

--- src/main/java/com/roy/finwise/service/WalletService.java
+++ src/main/java/com/roy/finwise/service/WalletService.java
@@ -4,12 +4,11 @@ import com.roy.finwise.dto.WalletRequest;
 import com.roy.finwise.dto.WalletResponse;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface WalletService {
-    WalletResponse createWallet(WalletRequest walletRequest, String userId);
-    Optional<WalletResponse> getWalletById(String walletId);
-    List<WalletResponse> getAllWallets();
+    WalletResponse createWallet(WalletRequest walletRequest);
+    WalletResponse getWalletById(String walletId);
+    List<WalletResponse> getAllWallets(String userId);
     WalletResponse updateWallet(String walletId, WalletRequest walletRequest);
     void deleteWallet(String walletId);
 }
