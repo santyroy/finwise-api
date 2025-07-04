ef42ed5af2565b9231d9d7bc2a9f5a61896017f5
Sougata Roy <sougata.roy26@yahoo.com>
2025-07-04 23:05:23 +0530

    Fix unused imports

--- src/main/java/com/roy/finwise/dto/UserRequest.java
+++ src/main/java/com/roy/finwise/dto/UserRequest.java
@@ -1,6 +1,5 @@
 package com.roy.finwise.dto;
 
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.*;
